16a7e23a9090f3620540953b854f964c3bc846a4
Joao Pedro Alcantara Gomes <joaopedro@alcantaragomes.com>
2025-06-18 19:58:13 -0300

    Avancado

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import streamlit as st
 import random as rdm
 
-st.title("Gerador de Senhas")
+st.title("Gerador de Senhas 🔐")
 st.header("Customize sua senha")
 
 tamanho=st.slider("Tamanho da senha",8,32,12)
-senha=''
-for i in range(tamanho):
-    escolha=rdm.choice(1,2)
-    if escolha==1:
-        caractere=rdm.choice("abcdefghijklmnopq")
-st.button("Criar Senha")
-st.code(senha_gerada)
-#teste
+
+def criar_senha(tamanho):
+    senha=''
+    for i in range(tamanho):
+        opcoes=[1,2,3]
+        escolha=rdm.choice(opcoes)
+        if escolha==1:
+            caractere=rdm.choice("abcdefghijklmnopqrstuwxyz")
+        elif escolha==2:
+            caractere=rdm.randint(0,9)
+        elif escolha==3:
+            caractere=rdm.choice("ABCDEFGHIJKLMNOPQRSTUWXYZ")
+        senha+=str(caractere)
+    return senha
+
+if st.button("Criar Senha"):
+    st.session_state.senha_gerada = criar_senha(tamanho)
+
+if "senha_gerada" in st.session_state:
+    st.success(f"Senha gerada: {st.session_state.senha_gerada}")
+#O f"..." serve para utilizarmos uma string e, dentro dela, utilizarmos uma variável, chamando a variável com {}
