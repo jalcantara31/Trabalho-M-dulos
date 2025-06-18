9ba9f4b2f2df4d1013e177e78f74dbc967bd4bb8
Joao Pedro Alcantara Gomes <joaopedro@alcantaragomes.com>
2025-06-18 16:56:43 -0300

    ç

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import random as rdm
 
-st.write("Olá, Stela")
-st.write("Eu já expliquei")
+st.title("Gerador de Senhas")
+st.header("Customize sua senha")
+
+tamanho=st.slider("Tamanho da senha",8,32,12)
+senha=''
+for i in range(tamanho):
+    escolha=rdm.choice(1,2)
+    if escolha==1:
+        caractere=rdm.choice("abcdefghijklmnopq")
+st.button("Criar Senha")
+st.code(senha_gerada)
